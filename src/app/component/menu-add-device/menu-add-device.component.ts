a0cb0e848221065fe4a42fe05042cd49980f4846
Kirana <thanna753@gmail.com>
2019-06-12 11:19:20 +0700

    Change menu-add-device

--- src/app/component/menu-add-device/menu-add-device.component.ts
+++ src/app/component/menu-add-device/menu-add-device.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-add-device.component.css']
 })
 export class MenuAddDeviceComponent implements OnInit {
-
+  inName = "";
+  inSerial = "";
+  inSpec = "";
+  inImg = "";
+  inStatus = "";
+  inHolder = "";
+  
   constructor() { }
 
   ngOnInit() {
