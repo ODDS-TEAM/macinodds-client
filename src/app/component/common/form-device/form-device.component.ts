c513fd21f86dd9d6d69b0b3529e60a6116ff3cf1
sivaroot <sivaroot.sut@gmail.com>
2019-07-11 18:09:48 +0700

    [edit] fix form data

--- src/app/component/common/form-device/form-device.component.ts
+++ src/app/component/common/form-device/form-device.component.ts
@@ -10,7 +10,7 @@ import { LyResizingCroppingImages, ImgCropperConfig, ImgResolution } from '@alyl
 import { LyTheme2 } from '@alyle/ui';
 import { AutofillMonitor } from '@angular/cdk/text-field';
 
-import { formatDate } from '@angular/common';
+
 
 
 
@@ -25,10 +25,8 @@ const styles = {
   },
   flex: {
     flex: 1
-  },
-  range: {
-    textAlign: 'center'
   }
+
 };
 
 
@@ -41,14 +39,9 @@ const styles = {
 })
 export class FormDeviceComponent implements OnInit {
 
-
-
+  readonly imageDefaultPath: string = '/assets/imgs/add_device.jpg';
 
   addDeviceForm: FormGroup;
-
-
-
-
 
   public results: any; // กำหนดตัวแปร เพื่อรับค่า
 
@@ -62,33 +55,35 @@ export class FormDeviceComponent implements OnInit {
   CardHeaderLabel = 'ลงทะเบียนอุปกรณ์';
 
 
+  backupData: any = {
+    name: '',
+    serial: '',
+    spec: '',
+    // memo: '',
+    img: '',
+    location: '',
+    status: true
+  };
+
   data: any = {
     name: '',
     serial: '',
     spec: '',
-    memo: '',
-    img: '',
-    location: ''
+    // memo: '',
+    imgs: null,
+    location: '',
+    status: true
+
   };
 
 
-  name: string;
-  serial: string;
-  spec: string;
-  image = '';
-  checker: string;
-  status: boolean;
-  holder: string;
-  tel: string;
-  imageDefault = '/assets/imgs/add_device.jpg';
+  imageDefault = this.imageDefaultPath;
   fileToUpload: File = null;
   vaildatBT = false;
-  options: FormGroup;
   fileName: string;
   fileNameEventInput: string;
-
-
   base64DefaultURL: any;
+
   // Set size image at cropping modal
   classes = this.theme.addStyleSheet(styles);
   croppedImage?: string;
@@ -96,13 +91,13 @@ export class FormDeviceComponent implements OnInit {
 
   myConfig: ImgCropperConfig = {
 
-    // autoCrop: true,
+    autoCrop: false,
     // extraZoomOut: true,
     width: 300, // Default `250`
     height: 300, // Default `200`
     fill: '#fff', // Default transparent if type = png else #000,
     // type: 'image/jpeg',
-    output: ImgResolution.OriginalImage // Default ImgResolution.Default
+    extraZoomOut: true
   };
 
   // End set size image at cropping modal
@@ -113,9 +108,8 @@ export class FormDeviceComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     private theme: LyTheme2,
-    private macApiService: MacinoddsApiService,
-    private dialog: MatDialog) {
-    this.imageDefault === '/assets/imgs/add_device.jpg' ? this.vaildatBT = false : this.vaildatBT = true;
+    private macApiService: MacinoddsApiService) {
+    this.imageDefault === this.imageDefaultPath ? this.vaildatBT = false : this.vaildatBT = true;
 
 
   }
@@ -125,23 +119,9 @@ export class FormDeviceComponent implements OnInit {
 
   ngOnInit() {
     this.createForm();
-    // this.getDeviceById(this.idEditDevice);
-    // this.getDevice();
-    /* this.onSelectionChange();*/
-
     this.componentSet(this.idEditDevice);
   }
 
-
-  // getDevice() {
-  //   // HTTP request by get() method
-  //   // get data from Observable we need subscibe observer to working
-  //   this.http.get('http://mac.odds.team/api/mac').subscribe(data => {
-  //     // get result from JSON response
-  //     this.results = data;
-  //     console.log(this.result);
-  //   });
-  // }
   private createForm() {
     this.addDeviceForm = this.formBuilder.group({
       nameDevice: ['', Validators.required],
@@ -150,8 +130,8 @@ export class FormDeviceComponent implements OnInit {
       location: [{ value: 'Hard code', disabled: this.editCompoCheck }, [Validators.required, Validators.minLength(1)]],
     });
   }
+
   onCropped(e) {
-    console.log('eeee')
     this.croppedImage = e.dataURL;
     const cropNew = this.croppedImage.replace(/^data:image\/(png|jpeg);base64,/, '');
     console.log((cropNew));
@@ -167,7 +147,9 @@ export class FormDeviceComponent implements OnInit {
     this.imageDefault = this.croppedImage;
     this.vaildatBT = true;
     // this.canSubmit();
-
+    console.log("fileupload ====> " + this.fileToUpload);
+    this.data.img = imageFile;
+    console.log("file data ====> " + this.data.img);
   }
 
   dataURItoBlob(dataURI) {
@@ -181,51 +163,45 @@ export class FormDeviceComponent implements OnInit {
     return blob;
   }
 
-  uploadCropImg() {
-
-    document.getElementById('upload-crop-img').click();
-
-  }
-
-  testEvent($event) {
-    console.log($event);
-  }
-
   componentSet(id) {
     console.log(id);
     if (this.editCompoCheck) {
       this.btnLabel = 'Cancel';
       this.CardHeaderLabel = 'แก้ไขอุปกรณ์';
-      this.macApiService.getMacIDApi(id).subscribe(data => {
-        console.log(data);
-        this.data = data;
+      this.macApiService.getMacIDApi(id).subscribe(res => {
+        this.data = res;
+        this.backupData = Object.assign({}, this.data);
         this.imageDefault = 'http://139.5.146.213/assets/imgs/devices/' + this.data.img;
         this.fileName = this.data.img;
-        console.log(this.data.img)
-        // this.data.location = 'Hard code location for test';
       });
     } else {
       this.CardHeaderLabel = 'ลงทะเบียนอุปกรณ์';
       this.btnLabel = 'Clear';
-      this.imageDefault = '/assets/imgs/add_device.jpg';
+      this.imageDefault = this.imageDefaultPath;
+      this.vaildatBT = false;
     }
-
   }
+
   openUploadModal(e) {
     const fileName = e.srcElement.value.toString().split("\\");
     this.fileNameEventInput = fileName[fileName.length - 1];
     document.getElementById('openUploadModal').click();
-
   }
+
   cancel() {
+    // edit form
     if (this.editCompoCheck) {
       this.router.navigate(['/admin/app/menu-view-admin']);
-
-    } else {
+    }
+    // add form 
+    else {
       this.addDeviceForm.reset();
-      this.imageDefault = '/assets/imgs/add_device.jpg';
+      this.imageDefault = this.imageDefaultPath;
+      this.fileName = '';
+      this.vaildatBT = false;
     }
   }
+
   onSubmit() {
     // create formData to post
     const formData: FormData = new FormData();
@@ -234,152 +210,39 @@ export class FormDeviceComponent implements OnInit {
     formData.append('spec', this.data.spec);
     formData.append('status', 'true');
     formData.append('img', this.fileToUpload);
-    // formData.append('holder', this.holder);
-    // formData.append('tel', this.tel);
-    // console.log(this.tel);
-    console.log(JSON.stringify(formData));
-    // post method
-    if (window.confirm('ยืนยันการบันทึกข้อมูล')) {
-      this.http.post('http://mac.odds.team/api/mac', formData)
-        .subscribe(result => {
-          console.log(result);
-          this.cancel();
-          this.router.navigate(['/admin/app/menu-view-admin']);
-        });
-    }
-  }
-  /*
-    onSelectionChange() {
-      if (this.checker === 'false') {
-        this.status = false;
-        this.holder = '';
-        this.tel = '';
-        this.canSubmit();
-      } else {
-        this.status = true;
-        this.holder = '';
-        this.tel = '';
-        this.canSubmit();
-      }
-    }
-    createForm() {
-      this.options = this.formBuilder.group({
-        name: '',
-        serial: '',
-        spec: '',
-        image: '',
-        status: '',
-        holder: '',
-        tel: ''
-      });
-    }
-    getDevice() {
-      // HTTP request by get() method
-      // get data from Observable we need subscibe observer to working
-      this.http.get('http://mac.odds.team/api/mac').subscribe(data => {
-        // get result from JSON response
-        this.results = data;
-      });
-    }
 
-    onSubmit() {
-      // create formData to post
-      const formData: FormData = new FormData();
-      formData.append('name', this.name);
-      formData.append('serial', this.serial);
-      formData.append('spec', this.spec);
-      formData.append('status', this.status.toString());
+    // check page is edit
+    if (this.editCompoCheck) {
       formData.append('img', this.fileToUpload);
-      formData.append('holder', this.holder);
-      formData.append('tel', this.tel);
-      console.log(this.tel);
-      console.log(JSON.stringify(formData));
+      this.macApiService.putMacAPI(this.idEditDevice, formData).subscribe(data => {
+        this.router.navigate(['/admin/app/menu-view-admin']);
+      });
+    } else {
+      // page is add device
       // post method
       if (window.confirm('ยืนยันการบันทึกข้อมูล')) {
         this.http.post('http://mac.odds.team/api/mac', formData)
           .subscribe(result => {
-            this.getDevice();
             console.log(result);
-            this.resetForm();
-            this.imageDefault = '/assets/imgs/add_image_icon.png';
+            this.cancel();
             this.router.navigate(['/admin/app/menu-view-admin']);
           });
       }
     }
-  
-    // method for reset data in field
-    resetForm() {
-      this.options.reset();
-      this.status = true;
-      this.image = '';
-      this.imageDefault = '/assets/imgs/add_image_icon.png';
-      console.log('clear');
-    }
-  
-  
-    checkPhoneNum(event: any) {
-      const pattern = /[0-9]/;
-  
-      const inputChar = String.fromCharCode(event.charCode);
-      if (event.keyCode !== 8 && !pattern.test(inputChar)) {
-        event.preventDefault();
-      }
-    }
-    // check input in form
-    canSubmit() {
-      if (this.name && this.serial && this.spec && this.fileToUpload && this.status === false && this.holder && this.tel) {
-        // is not empty check unavalible
-        this.vaildatBT = true;
-  
-      } else if (this.name && this.serial && this.spec && this.fileToUpload && this.status === true && !this.holder && !this.tel) {
-        // is not empty check avalible
-        this.vaildatBT = true;
-  
-      } else {
-        // is empty
-        this.vaildatBT = false;
-      }
-  
-    }
-  
-  
-    // Click cropped
-    // This use function have covert dataURL to file for add image cropped to imageFile (Send to API)
-    onCropped(e) {
-      this.croppedImage = e.dataURL;
-      console.log(typeof (this.croppedImage));
-      const cropNew = this.croppedImage.replace(/^data:image\/(png|jpg);base64,/, '');
-      const date = new Date().valueOf();
-      const text = '';
-      const imageName = date + '.' + text + '.jpeg';
-      const imageBlob = this.dataURItoBlob(cropNew);
-      const imageFile = new File([imageBlob], imageName, { type: 'image/jpeg' });
-      console.log(typeof (imageFile));
-  
-      // this.fileToUpload = imageFile;
-      this.fileToUpload = imageFile;
-      this.imageDefault = this.croppedImage;
-      this.canSubmit();
-    }
-  
-  
-    // function for convert dataURL to file
-    dataURItoBlob(dataURI) {
-      const byteString = window.atob(dataURI);
-      const arrayBuffer = new ArrayBuffer(byteString.length);
-      const int8Array = new Uint8Array(arrayBuffer);
-      for (let i = 0; i < byteString.length; i++) {
-        int8Array[i] = byteString.charCodeAt(i);
-      }
-      const blob = new Blob([int8Array], { type: 'image/jpeg' });
-      return blob;
-    }
-  
-    // onclick upload btn to open modol and click button upload image
-    uploadCropImg() {
-      document.getElementById('upload-crop-img').click();
-    }
-  
-  */
+  }
+
+  // validat จ้า
+  validatorName() {
+    this.data.name !== this.backupData.name ? this.vaildatBT = true : this.vaildatBT = false;
+  }
+  validatorSerial() {
+    this.data.serial !== this.backupData.serial ? this.vaildatBT = true : this.vaildatBT = false;
+  }
+  validatorSpec() {
+    this.data.spec !== this.backupData.spec ? this.vaildatBT = true : this.vaildatBT = false;
+  }
+  validatorImg() {
+    this.data.img !== this.backupData.img ? this.vaildatBT = true : this.vaildatBT = false;
+  }
 
 }
