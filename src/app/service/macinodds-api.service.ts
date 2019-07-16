c26d2bb5118b3beb740c36abe3158779a6206389
Anusorn <p_anusorn@kkumail.com>
2019-07-16 16:56:06 +0700

    [Test] API

--- src/app/service/macinodds-api.service.ts
+++ src/app/service/macinodds-api.service.ts
@@ -15,6 +15,8 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class MacinoddsApiService {
+  //  urlPath = "https://atb-macinodds.herokuapp.com/devices";
+   urlPath = 'https://5d008336d021760014b74fa8.mockapi.io/test/macs/';
 
   listData: User[] = [];
   siteName: string;
@@ -106,25 +108,26 @@ export class MacinoddsApiService {
   }
 
 
+  
 // Mock API
   getMacApi() {
-    return this.http.get('https://5d008336d021760014b74fa8.mockapi.io/test/macs');
+    return this.http.get(this.urlPath);
   }
 
   getMacIDApi(id) {
-    return this.http.get('https://5d008336d021760014b74fa8.mockapi.io/test/macs/' + id);
+    return this.http.get(this.urlPath +'/' + id);
   }
 
   putMacAPI(id, data) {
-    return this.http.put('https://5d008336d021760014b74fa8.mockapi.io/test/macs/' + id, data);
+    return this.http.put(this.urlPath +'/' + id, data);
   }
 
   postMacAPI(data) {
-    return this.http.post('https://5d008336d021760014b74fa8.mockapi.io/test/macs', data);
+    return this.http.post(this.urlPath, data);
   }
   
   deleteMacAPI(id) {
-    return this.http.delete('https://5d008336d021760014b74fa8.mockapi.io/test/macs/' + id);
+    return this.http.delete(this.urlPath +'/' + id);
   }
 
 //Real API
