dee7f53efb3fc6acdb1539fbf00967bb70780e8c
Punyawee Wijitkosum <punyawee_w@odds.team>
2019-07-17 13:47:57 +0700

    setup & getrole

--- src/app/first-login/first-login.component.ts
+++ src/app/first-login/first-login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService, SocialUser } from 'angular-6-social-login';
 import { Router } from '@angular/router';
-import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
 import { MatRadioButton, MatCardModule, MatDialog } from '@angular/material';
+import { User } from '../shared/user';
+import { MacinoddsApiService } from '../service/macinodds-api.service';
 
 
 @Component({
@@ -12,14 +14,19 @@ import { MatRadioButton, MatCardModule, MatDialog } from '@angular/material';
 })
 export class FirstLoginComponent implements OnInit {
 
+  macinoddsService: MacinoddsApiService;
+  private user: SocialUser;
+  // users: User;
+  id = sessionStorage.getItem('idUser');
+  role = sessionStorage.getItem('role');
+  firstLoginForm: FormGroup;
+
   constructor(
     private authService: AuthService,
     private router: Router,
+    private formBuild: FormBuilder,
     // private dialog: MatDialog,
   ) { }
-
-  private user: SocialUser;
-  firstLoginForm: FormGroup;
 
   nextPage() {
     console.log('valid no get = ' + this.firstLoginForm.valid)
@@ -28,25 +35,67 @@ export class FirstLoginComponent implements OnInit {
     console.log('status = ' + this.firstLoginForm.get('telephoneNumb').status)
     console.log('valid = ' + this.firstLoginForm.get('telephoneNumb').valid)
     console.log('validator.length = ' + this.firstLoginForm.get('telephoneNumb').validator.length)
+
+    const { fullName, emailODDS, slackAccount, telephoneNumb } = this.firstLoginForm.getRawValue();
+
+    // tslint:disable-next-line:max-line-length
+    if (telephoneNumb && slackAccount
+    ) {
+      //   if (this.role === 'corporate') {
+      //   if (corporateName === '') {
+      //     alert('Please complete the information.');
+      //     return;
+      //   }
+      //   this.user.corporateName = corporateName;
+      // }
+      // this.user.firstName = firstName;
+      // this.user.lastName = lastName;
+      // this.user.bankAccountName = bankAccountName;
+      // this.user.bankAccountNumber = bankAccountNumber;
+      // this.user.slackAccount = slackAccount;
+      // this.user.role = this.role;
+      // this.user.vat = this.vat;
+      // this.user.siteId = siteId;
+      // this.user.project = project;
+      // this.updateUser();
+    } else {
+      alert('Please complete the information.');
+    }
+
+    console.log(this.role);
+
     if (this.firstLoginForm.get('slackAccount').valid && this.firstLoginForm.get('telephoneNumb').valid) {
-      this.router.navigate(['/admin/app/menu-view-admin']);
+       if (this.role === 'admin') {
+        this.router.navigate(['/admin']);
+      } else {
+        this.router.navigate(['/user']);
+      }
     }
   }
 
   ngOnInit() {
-    this.authService.authState.subscribe((user) => {
-      this.user = user;
+    console.log(this.user + '<<<<<<');
+    this.setup();
+
+    this.authService.authState.subscribe((users) => {
+      this.user = users;
       // this.loggedIn = (user != null); 
-
+      console.log(this.user);
     });
-    this.firstLoginForm = new FormGroup({
-      'fullName': new FormControl({ value: '', disabled: true }, Validators.required),
-      'emailODDS': new FormControl({ value: '', disabled: true }, Validators.required),
-      'slackAccount': new FormControl('', [Validators.email, Validators.required]),
-      'telephoneNumb': new FormControl('', [Validators.min(10), Validators.pattern('[0-9]{10}'), Validators.required])
+  }
+
+  setup() {
+    this.firstLoginForm  = new FormGroup({
+      fullName: new FormControl({ value: '', disabled: true }, Validators.required),
+      emailODDS: new FormControl({ value: '', disabled: true }, Validators.required),
+      slackAccount: new FormControl('', [Validators.email, Validators.required]),
+      telephoneNumb: new FormControl('', [Validators.min(10), Validators.pattern('[0-9]{10}'), Validators.required])
     });
-
-
+  }
+  getRole() {
+    this.macinoddsService.getRoleUser(this.id).subscribe(res => {
+      this.role = res.role;
+    })
   }
 
 }
