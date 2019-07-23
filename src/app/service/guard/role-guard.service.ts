f0282dcd23ae65b13e0ac9d81d9369a073358698
Punyawee Wijitkosum <punyawee_w@odds.team>
2019-07-23 11:32:22 +0700

    [ADD]token + comment

--- src/app/service/guard/role-guard.service.ts
+++ src/app/service/guard/role-guard.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { MacinoddsApiService } from '../macinodds-api.service';
 import { Observable } from 'rxjs';
-import { decode } from 'punycode';
+import * as JWT from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +18,20 @@ export class RoleGuardService implements CanActivate {
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
     const expectedRole = next.data.expectedRole;
-  
-    if (this.role !== expectedRole) {
+    const token = sessionStorage.getItem('token');
+    const decode = JWT(token);
+    // const tokenPayload = decode(token);
+
+    if ( this.role !== expectedRole) {
       this.route.navigate(['/404']);
+      // console.log('tokenplayload +++++++++++++++++ ' + tokenPayload);
 
       return false;
     } 
+    // console.log('tokenplayload.role +++++++++++++++++ ' + tokenPayload.role);
+    // // console.log('tokenplayload +++++++++++++++++ ' + tokenPayload.role);
+    // console.log('tokenplayload +++++++++++++++++ ' + tokenPayload);
+
     return true;
     
   }
