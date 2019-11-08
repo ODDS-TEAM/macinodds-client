b13a0dbc8f9506634c7f8670de66d56823d90a2a
aphiratnimanussonkul <b5923151@gmail.com>
2019-11-08 15:44:14 +0700

    fixed bug role

--- src/app/service/guard/role-guard.service.ts
+++ src/app/service/guard/role-guard.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { MacinoddsApiService } from '../macinodds-api.service';
 import { Observable } from 'rxjs';
-import * as JWT from 'jwt-decode';
-import { Token } from 'src/app/shared/token';
 import { CheckRoleTokenService } from '../check-role-token.service';
 
 @Injectable({
@@ -17,10 +15,7 @@ export class RoleGuardService implements CanActivate {
   ) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    let tokenDecode: Token;
     const expectedRole = next.data.expectedRole;
-    const token = sessionStorage.getItem('token');
-    tokenDecode =  JWT(token);
     if (this.checkRoleToken.checkRoleByToken() !== expectedRole) {
       if(this.checkRoleToken.checkRoleByToken() === 'admin') {
         this.route.navigate(['/admin'])
